6d9fa6542d49be0466e1ef082ea88e1a1f9e8563
n-speech <dafna5566@gmail.com>
2025-11-30 21:45:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+body {
+  font-family: Arial, sans-serif;
+  background: #FFFBE8;
+  text-align: center;
+}
+
+.login-box {
+  max-width: 300px;
+  margin: 80px auto;
+  padding: 20px;
+  background: white;
+  border-radius: 15px;
+}
+
+input, button {
+  width: 100%;
+  padding: 12px;
+  margin: 8px 0;
+  font-size: 18px;
+  border-radius: 10px;
+}
